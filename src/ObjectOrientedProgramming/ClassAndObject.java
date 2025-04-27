1817a8af0a1cbdb2d61cf6de8d82cae1f488b446
Kunal Lad <kunallad0007@gmail.com>
2025-04-27 12:50:27 +0530

    Optimizing code

--- src/ObjectOrientedProgramming/ClassAndObject.java
+++ src/ObjectOrientedProgramming/ClassAndObject.java
@@ -1,12 +1,28 @@
 package ObjectOrientedProgramming;
 
+// Basic way of writing the code
 class Car{
     public void carName(String name){
-//        String test = "Test";
         System.out.println("Car Name: " + name);
     }
     public void carEngine(String engine){
         System.out.println("Car Engine: " + engine);
+    }
+}
+
+//more clean of wanted to do only this things then we can access the class using constructor
+
+class Animal{
+    String animalName;
+    String animalBreed;
+    Animal(String animalName, String animalBreed){
+        this.animalName = animalName;
+        this.animalBreed = animalBreed;
+    }
+
+    public void display(){
+        System.out.println("Animal Name: " + animalName);
+        System.out.println("Animal Breed: " + animalBreed);
     }
 }
 
@@ -21,6 +37,13 @@ public class ClassAndObject {
        car2.carName("Ferrari");
        car2.carEngine("F136 Engine");
 
-//       car1.carName("test car").test;
+//     Now we can directly call the class using the constructor
+       Animal animal1 = new Animal("Lion", "Barbary Lion");
+       Animal animal2 = new Animal("Deer", "Barasingha");
+
+//       Now, let's print
+       animal1.display();
+       animal2.display();
+
     }
 }
