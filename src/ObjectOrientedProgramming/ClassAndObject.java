fe09efda073733804f0aff5d8885141fd505d0e8
Kunal Lad <kunallad0007@gmail.com>
2025-04-27 12:42:56 +0530

    Updated the file with basic program

--- src/ObjectOrientedProgramming/ClassAndObject.java
+++ src/ObjectOrientedProgramming/ClassAndObject.java
@@ -1,4 +1,26 @@
 package ObjectOrientedProgramming;
 
+class Car{
+    public void carName(String name){
+//        String test = "Test";
+        System.out.println("Car Name: " + name);
+    }
+    public void carEngine(String engine){
+        System.out.println("Car Engine: " + engine);
+    }
+}
+
 public class ClassAndObject {
+    public static void main(String[] args) {
+       Car car1 = new Car();
+       Car car2 = new Car();
+
+       car1.carName("BMW");
+       car1.carEngine("S55 Engine");
+
+       car2.carName("Ferrari");
+       car2.carEngine("F136 Engine");
+
+//       car1.carName("test car").test;
+    }
 }
