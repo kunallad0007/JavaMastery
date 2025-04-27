38840f39373da0dddd661d10bfbf97727d6a0ab1
Kunal Lad <kunallad0007@gmail.com>
2025-04-27 21:04:59 +0530

    Updated class names

--- src/ObjectOrientedProgramming/ClassAndObject.java
+++ src/ObjectOrientedProgramming/ClassAndObject.java
@@ -1,7 +1,7 @@
 package ObjectOrientedProgramming;
 
 // Basic way of writing the code
-class Car{
+class Car1{
     public void carName(String name){
         System.out.println("Name: " + name);
     }
@@ -12,10 +12,10 @@ class Car{
 
 //more clean of wanted to do only this things then we can access the class using constructor
 
-class Animal{
+class Animal1{
     String animalName;
     String animalBreed;
-    Animal(String animalName, String animalBreed){
+    Animal1(String animalName, String animalBreed){
         this.animalName = animalName;
         this.animalBreed = animalBreed;
     }
@@ -29,8 +29,8 @@ class Animal{
 
 public class ClassAndObject {
     public static void main(String[] args) {
-       Car car1 = new Car();
-       Car car2 = new Car();
+       Car1 car1 = new Car1();
+       Car1 car2 = new Car1();
        System.out.println("--- Car Details ---");
        car1.carName("BMW");
        car1.carEngine("S55 Engine");
@@ -39,8 +39,8 @@ public class ClassAndObject {
        car2.carEngine("F136 Engine");
 
 //     Now we can directly call the class using the constructor
-       Animal animal1 = new Animal("Lion", "Barbary Lion");
-       Animal animal2 = new Animal("Deer", "Barasingha");
+       Animal1 animal1 = new Animal1("Lion", "Barbary Lion");
+       Animal1 animal2 = new Animal1("Deer", "Barasingha");
 
 //       Now, let's print
        animal1.display();
